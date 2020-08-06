2443db9715f13f13eeb76c36029274baa6bc5461
petros <pit.trak@gmail.com>
2020-08-06 13:51:54 +0300

    Form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,28 @@
 const socket = io()
+// elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
 
 socket.on('message', (message) => {
     console.log(message);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    // disable button
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message, (error) => {
-        if(error){
+        // enable button
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
             console.log(error);
         }
 
@@ -16,16 +30,20 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
-    if(!navigator.geolocation){
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
 
+    //disable button
+    $sendLocationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sendLocation', { 
-            latitude: position.coords.latitude, 
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            //enable button
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location Shared');
         })
     })
