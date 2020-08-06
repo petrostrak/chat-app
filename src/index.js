58c5b619a6faa357fff37bc012ecf902cc3faaea
petros <pit.trak@gmail.com>
2020-08-06 11:47:32 +0300

    Socket.io events

--- src/index.js
+++ src/index.js
@@ -7,15 +7,27 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
-    console.log('New WebSocket connection');
+let count = 0
+
+// SERVER (emit) -> CLIENT (receive) - countUpdate
+// CLIENT (emit) -> SERVER (receive) - increment
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
-    console.log('Server is up on port ' + port);
+    console.log(`Server is up on port ${port}!`)
 })
