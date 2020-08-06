6e4e8a33919f1e196cfbbd34c046be171c7eb1c8
petros <pit.trak@gmail.com>
2020-08-06 14:34:57 +0300

    Rendering location messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,10 +8,18 @@ const $messages = document.querySelector('#messages')
 
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#message-url').innerHTML
 
 socket.on('message', (message) => {
     const html = Mustache.render(messageTemplate, {
         message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+    const html = Mustache.render(locationTemplate, {
+        url
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
